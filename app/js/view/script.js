443673a320200b02a4c7ca318c81d04d033f53f1
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-11-04 08:50:27 +0100

    Add POST api for adding books

--- app/js/view/script.js
+++ app/js/view/script.js
@@ -7,8 +7,53 @@ var Book = function (book) {
 }
 
 var ViewModel = function () {
+
+    $(document).ajaxStart(function () {
+        console.log("Ajax Request is Starting");
+    });
+    // Ajax stop global function
+    $(document).ajaxStop(function () {
+        console.log("Ajax Request has ended");
+    });
+
     var self = this;
+
+    //apply bindings to the form input
+    self.bookName = ko.observable();
+    self.author = ko.observable();
+    self.isbn = ko.observable();
+    self.description = ko.observable();
+
+    //store books fetched from the API
     self.books = ko.observableArray([]);
+
+    //add book to database via API
+    self.addBook = function () {
+        if (!self.bookName() || !self.author() || !self.isbn() || !self.description()) {
+            alert("All fields required");
+        } else {
+            //build an object to add to database via API
+            var book = {
+                "name": self.bookName(),
+                "author": self.author(),
+                "isbn": self.isbn(),
+                "description": self.description()
+            }
+            $.ajax({
+                type: "POST",
+                crossOrigin: true,
+                contentType: 'application/json; charset=UTF-8',
+                url: "/api/book/",
+                data: JSON.stringify(book),
+                processData: true,
+                dataType: "json"
+            }).done(
+                self.books.push(new Book(book))
+            ).fail(function errorFn(xhr, status, strErr) {
+                console.log('There seems to be an error in adding books' + strErr);
+            });
+        }
+    }
 
     // make an api call to get books then pull the books out of the result 
     //of the api call and make them observables to be displayed on the index page
@@ -25,7 +70,7 @@ var ViewModel = function () {
             }
         }
     ).fail(function errorFn(xhr, status, strErr) {
-        console.log('There seems to be an Error in connection' + strErr);
+        console.log('There seems to be an error in retrieving books' + strErr);
     });
 }
 ko.applyBindings(new ViewModel());
